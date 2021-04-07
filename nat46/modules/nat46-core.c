6b5bfd9747ec4315463b1ab638c0bb697db83e24
Richard Patterson <richard@helix.net.nz>
2021-04-07 15:14:18 +0100

    skip generating a UDP4 csum if the UDP6 csum is 0 and zero_csum_pass is set

--- nat46/modules/nat46-core.c
+++ nat46/modules/nat46-core.c
@@ -1576,6 +1576,10 @@ void nat46_ipv6_input(struct sk_buff *old_skb) {
         }
       case NEXTHDR_UDP: {
         struct udphdr *udp = add_offset(ip6h, v6packet_l3size);
+        if ((udp->check == 0) && zero_csum_pass) {
+          /* zero checksum and the config to pass it is set - do nothing with it */
+          break;
+        }
         u16 sum1 = csum_ipv6_unmagic(nat46, &ip6h->saddr, &ip6h->daddr, l3_infrag_payload_len, NEXTHDR_UDP, udp->check);
         u16 sum2 = csum_tcpudp_remagic(v4saddr, v4daddr, l3_infrag_payload_len, NEXTHDR_UDP, sum1);
         udp->check = sum2;
